16c584c5e190a57fce36b35b8449b79f283c88a6
Bike <you@example.com>
2020-02-29 13:51:35 -0500

    February 29, 2020 updates

--- start.sh
+++ start.sh
@@ -7,7 +7,9 @@ TIME=$(date +"%F")
 #~~%I-%M-%S_%p")
 BIKEPATH=$(rospack find bike)/bagfiles
 
-# This command actually starts the ROS nodes found in the provided launch
-# config file. If you want a different config file to be used, pass the
+# This command starts the ROS nodes found in the provided launch
+# config file. If you want a different config file to be used, pass its
 # name in - e.g. "source start.sh nav" to use nav.launch as the file.
+#
+# The directory for the launch files is ~/ros_ws/src/bike/scripts/Launch/
 roslaunch bike ${1:-run_with_kalman}.launch time:=$TIME
